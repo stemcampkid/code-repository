64908b23c286e484ff2d5a7d99a928785e37138f
stemcampkid <milest5230@gmail.com>
2017-06-12 11:21:12 -0500

    Initial Commit

---
+++ Hello-world/Hello World Miles.c
@@ -0,0 +1,10 @@
+/* Miles Thompson
+ * Program #1
+ * Hello World
+ * 6/12/17
+ */
+#include "simpletools.h"                     
+int main()                                    
+{
+ print("Hello World");
+}
