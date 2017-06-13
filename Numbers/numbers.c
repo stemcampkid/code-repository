4e1452ee25f6cdb5db367cabbd56618a7af36a46
stemcampkid <milest5230@gmail.com>
2017-06-13 09:56:54 -0500

    Initial Commit

---
+++ Numbers/numbers.c
@@ -0,0 +1,20 @@
+/* Miles Thompson
+ * Program #3
+ * Number Project
+ * 6/13/17
+ */
+  
+#include "simpletools.h"                     
+
+int main()                                    
+{
+ int x = 8489795894378;
+ 
+ while(x < 555555555555556666555 )
+ {
+   print("%d\n", x);
+   x = x + 1;
+ }
+ 
+ print("Done!");
+}
