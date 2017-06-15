bf06c7ea936f7dc9908709de1f05f97303e3b1c7
stemcampkid <milest5230@gmail.com>
2017-06-15 09:52:31 -0500

    Initial Commit

---
+++ button game/fixed button game.c
@@ -0,0 +1,69 @@
+/* Miles Thompson
+   Program #5
+   Button game using a button and two LEDs
+   6/14/17
+*/
+#include "simpletools.h"                      
+
+int main()                                    
+{
+  
+int timeCounter = 0;
+char playAgain = 'Y';
+int interval;
+float averageScore = 0;
+int rounds = 0;
+int best = 1500;
+srand(CNT);
+
+print("Game Instructions: \n");
+print("The green LED will turn on when the game starts.\n");
+print("When the yellow LED turns on,\n");
+print("let go of the button as fast as you can.\n\n");
+
+while(playAgain == 'Y' || playAgain == 'y')
+ {
+   print("Press and hold the button to start. \n");
+   while(input(0) == 0)
+   { 
+     // intentionally empty loop
+   }  
+   high(15);
+   low(14);
+  
+  interval = (rand() % 1001) + 500;
+  pause(interval);
+  
+  low(15);
+  high(14);
+  
+  timeCounter = 0;
+  
+ while (input(0) == 1)
+  {
+    pause(1);
+    timeCounter = timeCounter + 1;
+  } 
+  low(14);
+  if(timeCounter > 0)
+  {
+    print("Your time was %d ms. \n", timeCounter);
+    averageScore = averageScore + timeCounter;
+    rounds = rounds + 1;
+    if (timeCounter < best)
+    {
+      best = timeCounter;
+    }     
+  }     
+  else
+  {
+    print(" You let go of the button too soon!\n");
+  }
+  print("Would you like to play again? (Y/n) ");
+  scan("%c", &playAgain);  
+ }
+
+ print("Goodbye! Your average score was %0.2f ms.\n", averageScore/rounds);
+ print("Your best time was %d ms.\n", best);
+ return 0;
+}
